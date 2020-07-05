58ed852127c7914a5eeeea8032a47437d7695f34
Eric Rojas Fuerte <erojas@labelgrup.com>
2020-07-05 16:57:56 +0200

    Add docs

---
+++ docs/precache-manifest.02d1b65c0696554d98bb29253bc321ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabe18a86e7528a303c2603ad1f0b013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9f714e2d20f1acd61d",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "38593c6eefd1feac64a5",
+    "url": "/static/css/main.b3e70373.chunk.css"
+  },
+  {
+    "revision": "3a9f714e2d20f1acd61d",
+    "url": "/static/js/2.2e8f2ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e8f2ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38593c6eefd1feac64a5",
+    "url": "/static/js/main.48845014.chunk.js"
+  },
+  {
+    "revision": "e88e71fbf84068e2cbab",
+    "url": "/static/js/runtime-main.30abb9fc.js"
+  }
+]);
